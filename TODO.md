e124eb6fbdd70144bbea240b19d69f05ab47c568
Brian Sajeev <briansajeev@gmail.com>
2023-10-29 23:48:02 +0530

    Made network parser more efficient
    Incorporated pointer vectors and pass by references

---
+++ TODO.md
@@ -0,0 +1,10 @@
+## To check later
+
+- weights in parser not working as expected
+- according to starter code, parents have string value but children have integer. Check again if that causes issues or inconsistencies.
+
+## To make
+
+- data updator (maximization step)
+- CPT updator (expectation step)
+- output file
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include <bits/stdc++.h>
 
 // hello
+// TODO
+// check parent string and children int
+
 // Format checker just assumes you have Alarm.bif and Solved_Alarm.bif (your file) in current directory
 using namespace std;
 
@@ -16,24 +19,28 @@ class Graph_Node
 {
 
 private:
-	string Node_Name;		// Variable name
-    int Node_Index; // Index of the node in the network
-	vector<int> Children;	// Children of a particular node - these are index of nodes in graph.
-	vector<string> Parents; // Parents of a particular node- note these are names of parents
-	int nvalues;			// Number of categories a variable represented by this node can take
-	vector<string> values;	// Categories of possible values
-	vector<float> CPT;		// conditional probability table as a 1-d array . Look for BIF format to understand its meaning
+	string Node_Name;	   // Variable name
+	int Node_Index;		   // Index of the node in the network
+	vector<int> Children;  // Children of a particular node - these are index of nodes in the graph.
+	vector<int> Parents;   // Parents of a particular node - note these are names of parents
+	int nvalues;		   // Number of categories a variable represented by this node can take
+	vector<string> values; // Categories of possible values
+	vector<float> CPT;	   // Conditional probability table as a 1-d array. Look for BIF format to understand its meaning
 
 public:
-	// Constructor- a node is initialised with its name and its categories
-	// Graph_Node(string name, int n, vector<string> vals)
+	// Constructor- a node is initialized with its name and its categories
 	Graph_Node(string name, int n, vector<string> vals, int index)
+		: Node_Name(name), Node_Index(index), Children(), Parents(), nvalues(n), values(vals), CPT()
 	{
-		Node_Name = name;
-		nvalues = n;
-		values = vals;
-		Node_Index = index;
 	}
+
+	// Graph_Node(string name, int n, vector<string> vals, int index)
+	// {
+	// 	Node_Name = name;
+	// 	nvalues = n;
+	// 	values = vals;
+	// 	Node_Index = index;
+	// }
 
 	string get_name()
 	{
@@ -43,7 +50,7 @@ public:
 	{
 		return Children;
 	}
-	vector<string> get_Parents()
+	vector<int> get_Parents()
 	{
 		return Parents;
 	}
@@ -64,21 +71,22 @@ public:
 		CPT.clear();
 		CPT = new_CPT;
 	}
-	void set_Parents(vector<string> Parent_Nodes)
+	void set_Parents(vector<int> Parent_Nodes)
 	{
 		Parents.clear();
 		Parents = Parent_Nodes;
 	}
 	// add another node in a graph as a child of this node
-	int add_child(int new_child_index)
+	void add_child(int new_child_index)
 	{
-		for (int i = 0; i < Children.size(); i++)
-		{
-			if (Children[i] == new_child_index)
-				return 0;
-		}
+		// Removed the looping below and assume added child is not already present
+		// for (int i = 0; i < Children.size(); i++)
+		// {
+		// 	if (Children[i] == new_child_index)
+		// 		return 0;
+		// }
 		Children.push_back(new_child_index);
-		return 1;
+		// return 1;
 	}
 };
 
@@ -86,88 +94,63 @@ public:
 class network
 {
 
-	list<Graph_Node> Pres_Graph;
-    map<string, int> node_id; // Maps node names to their indices
+private:
+	vector<Graph_Node *> Pres_Graph;
+	unordered_map<string, int> index_map; // Maps node names to their indices
 
 public:
-	// int addNode(Graph_Node node)
-	// {
-	// 	Pres_Graph.push_back(node);
-	// 	return 0;
-	// }
-    int addNode(Graph_Node node)
-    {
-        int index = Pres_Graph.size(); // Get the index of the added node
-        node_id[node.get_name()] = index; // Add the mapping to the map
-        Pres_Graph.push_back(node);
-        return 0;
-    }
+	// Constructor for the network class
+	network()
+		: Pres_Graph(), index_map()
+	{
+	}
+	void CPT_initializer();
+
+	int addNode(Graph_Node *node)
+	{
+		int index = Pres_Graph.size();		 // Get the index of the added node
+		index_map[node->get_name()] = index; // Add the mapping to the map
+		Pres_Graph.push_back(node);
+		return 0;
+	}
 
 	int netSize()
 	{
 		return Pres_Graph.size();
 	}
-	// get the index of node with a given name
-	int get_index(string val_name){
-		if (node_id.find(val_name) != node_id.end()) {
-			return node_id[val_name];
+	// Gets the index of node with a given name
+	int get_index(string val_name)
+	{
+		// TODO: can get rid of the map.find() part if it makes it faster
+		if (index_map.find(val_name) != index_map.end())
+		{
+			return index_map[val_name];
 		}
 		return -1; // Node not found
 	}
 
-	/*
-	int get_index(string val_name)
+	// Gets the pointer of node at nth index
+	Graph_Node *get_nth_node(int n)
 	{
-		list<Graph_Node>::iterator listIt;
-		int count = 0;
-		for (listIt = Pres_Graph.begin(); listIt != Pres_Graph.end(); listIt++)
-		{
-			if (listIt->get_name().compare(val_name) == 0)
-				return count;
-			count++;
-		}
-		return -1;
-	}*/
-	
-	// get the node at nth index
-	list<Graph_Node>::iterator get_nth_node(int n)
-	{
-		list<Graph_Node>::iterator listIt;
-		int count = 0;
-		for (listIt = Pres_Graph.begin(); listIt != Pres_Graph.end(); listIt++)
-		{
-			if (count == n)
-				return listIt;
-			count++;
-		}
-		return listIt;
-	} 
+		return Pres_Graph[n];
+	}
 
-	// get the iterator of a node with a given name
-	list<Graph_Node>::iterator search_node(string val_name)
+	// Gets the pointer of a node with a given name
+	Graph_Node *search_node(string val_name)
 	{
-		list<Graph_Node>::iterator listIt;
-		for (listIt = Pres_Graph.begin(); listIt != Pres_Graph.end(); listIt++)
-		{
-			if (listIt->get_name().compare(val_name) == 0)
-				return listIt;
-		}
-
-		cout << "node not found\n";
-		return listIt;
+		return Pres_Graph[index_map[val_name]];
+		// return get_nth_node(get_index(val_name));
 	}
 };
 
-
 network read_network()
 {
-	network Alarm;
+	network Graph_network = network();
 	string line;
 	int find = 0;
 	ifstream myfile("alarm.bif");
 	string temp;
 	string name;
-	vector<string> values;
 
 	if (myfile.is_open())
 	{
@@ -181,6 +164,7 @@ network read_network()
 
 			if (temp.compare("variable") == 0)
 			{
+				vector<string> values;
 
 				ss >> name;
 				getline(myfile, line);
@@ -199,133 +183,192 @@ network read_network()
 
 					ss2 >> temp;
 				}
-				Graph_Node new_node(name, values.size(), values);
-				int pos = Alarm.addNode(new_node);
+
+				int index = Graph_network.netSize(); // Get the index of the added node
+				Graph_Node *new_node = new Graph_Node(name, values.size(), values, index);
+				Graph_network.addNode(new_node);
+				// Graph_network.index_map[name] = pos;
 			}
 			else if (temp.compare("probability") == 0)
 			{
 
+				// temp represents name of variables.
 				ss >> temp;
 				ss >> temp;
+				// first variable name is the name of the current node.
+				Graph_Node *curr_node = Graph_network.search_node(temp);
+				int index = Graph_network.get_index(temp);
 
-				list<Graph_Node>::iterator listIt;
-				list<Graph_Node>::iterator listIt1;
-				listIt = Alarm.search_node(temp);
-				int index = Alarm.get_index(temp);
+				// remaining variable names are the name of the parents.
 				ss >> temp;
-				values.clear();
+				vector<int> parents;
 				while (temp.compare(")") != 0)
 				{
-					listIt1 = Alarm.search_node(temp);
-					listIt1->add_child(index);
-					values.push_back(temp);
+					// We find the parent node, modify its children list
+					// and add the parent name to the parents list of the current node.
+					Graph_Node *parent = Graph_network.search_node(temp);
+					parent->add_child(index);
+					parents.push_back(Graph_network.get_index(temp));
 
 					ss >> temp;
 				}
-				listIt->set_Parents(values);
+				curr_node->set_Parents(parents);
 				getline(myfile, line);
 				stringstream ss2;
 
 				ss2.str(line);
 				ss2 >> temp;
 
+				// Here temp represents each value stored in the CPT.
 				ss2 >> temp;
 
 				vector<float> curr_CPT;
-				string::size_type sz;
+				// string::size_type sz;
+
+				// iterate through and add each value to the CPT of the current node.
 				while (temp.compare(";") != 0)
 				{
-
 					curr_CPT.push_back(atof(temp.c_str()));
 
 					ss2 >> temp;
 				}
 
-				listIt->set_CPT(curr_CPT);
-			}
-			else
-			{
+				curr_node->set_CPT(curr_CPT);
 			}
 		}
 
+		//! what is the purpose of 'find' here? it is not modified anywhere above.
 		if (find == 1)
 			myfile.close();
 	}
 
-	return Alarm;
+	return Graph_network;
 }
 
-class data{
-    network Pres_Graph;
-    int total_variables; // Total number of variables in the data
-	vector<vector<int>> data; // Data is stored as integer format. Suppose a node can take 3 categories as values. Then first category is '0', then '1' and so on.
-	vector<int> missing;	  // Stores index of question mark for data i
+class Data
+{
+
+public:
+	network Data_network;
+	int total_variables;			// Total number of variables in the data
+	vector<vector<int>> data;		// Data is stored as integer format. Suppose a node can take 3 categories as values. Then first category is '0', then '1' and so on.
+	vector<int> missing;			// Stores index of question mark for data i
 	vector<vector<int>> datapoints; // Stores the useful data, after replacing '?' with possible values
 	vector<double> weights;			// Weights for the data that we will be using
-public:
 
-    void data::data_reader(){
-        string line;
-        ifstream myfile("filename1");
-        string temp;
-        string name;
+	Data(network Alarm)
+	{
+		Data_network = Alarm;
+		total_variables = Alarm.netSize();
+		data_reader();
+	};
 
-        if (!myfile.is_open())
-            return;
-        else
-        {
-            while (!myfile.eof())
-            {
-                stringstream ss;
-                getline(myfile, line);
-                vector<int> values_dataset; // Stores the values of the data
-                ss.str(line);
-                int missing_idx = -1; // Stores the index of the unknown value
-                for (int i = 0; i < total_variables; i++)
-                {
-                    ss >> temp;
-                    if (temp.compare("?") == 0)
-                    {
-                        missing_idx = i;
-                        values_dataset.push_back(-1);
-                    }
-                    else
-                    {
-                        int data_value = 0;
-                        // go through the list of values of the node
-                        int k = Pres_Graph.get_nth_node(i)->get_nvalues();
-                        for (int j = 0; j < k; j++)
-                        {
-                            if (temp.compare(Pres_Graph.get_nth_node(i)->get_values()[j]) == 0)
-                            {
-                                data_value = j;
-                                break;
-                            }
-                        }
-                        values_dataset.push_back(data_value);
+	void data_reader()
+	{
+		string line;
+		ifstream myfile("records.dat");
+		string temp;
+		string name;
 
-                    }
-                }
-                datapoints.push_back(values_dataset);
-                missing.push_back(missing_idx);
+		if (!myfile.is_open())
+			return;
+		else
+		{
+			while (!myfile.eof())
+			{
+				stringstream ss;
+				getline(myfile, line);
+				vector<int> values_dataset; // Stores the values of the data
+				ss.str(line);
+				int missing_idx = -1; // Stores the index of the unknown value
+				for (int i = 0; i < total_variables; i++)
+				{
+					ss >> temp;
+					// if (temp.compare("?") == 0)
+					// std::cout << temp << std::endl;
+					if (temp == "\"?\"")
+					{
+						missing_idx = i;
+						values_dataset.push_back(-1);
+					}
+					else
+					{
+						int data_value = 0;
+						// go through the list of values of the node
+						int k = Data_network.get_nth_node(i)->get_nvalues();
+						for (int j = 0; j < k; j++)
+						{
+							if (temp.compare(Data_network.get_nth_node(i)->get_values()[j]) == 0)
+							{
+								data_value = j;
+								break;
+							}
+						}
+						values_dataset.push_back(data_value);
+					}
+				}
+				datapoints.push_back(values_dataset);
+				missing.push_back(missing_idx);
 
-                if(missing_idx == -1){
-                    int node_idx = missing_idx;
-                    vector<int> probable_values;
-                    int k = Pres_Graph.get_nth_node(node_idx)->get_nvalues();
-                    for(int i = 0; i < k; i++){
-                        vector<int> probable_values(values_dataset.begin(), values_dataset.end());
-                        probable_values[missing_idx] = i ;
-                        data.push_back(probable_values);
-                        weights.push_back(1 / k);
-                    }
-                }else{
-                    data.push_back(values_dataset);
-                    weights.push_back(1);
-                }
-            }
-        }   
-    }
+				if (missing_idx != -1)
+				{
+					int node_idx = missing_idx;
+					vector<int> probable_values;
+					int k = Data_network.get_nth_node(node_idx)->get_nvalues();
+					for (int i = 0; i < k; i++)
+					{
+						vector<int> probable_values(values_dataset.begin(), values_dataset.end());
+						probable_values[missing_idx] = i;
+						data.push_back(probable_values);
+						weights.push_back(1 / k);
+					}
+				}
+				else
+				{
+					data.push_back(values_dataset);
+					weights.push_back(1);
+				}
+			}
+		}
+	}
 };
 
-void CPT_initializzer()
+void network::CPT_initializer()
+{
+	for (Graph_Node *listIt : Pres_Graph)
+	{
+		// Graph_Node *ListIt = ;
+		int nvalues = listIt->get_nvalues();
+		vector<float> CPT;
+		vector<int> Parents = listIt->get_Parents();
+		int num_parents = Parents.size();
+		int num_combinations = nvalues;
+		for (int i = 0; i < num_parents; i++)
+		{
+			Graph_Node *parent = get_nth_node(Parents[i]);
+			int n = parent->get_nvalues();
+			num_combinations *= n;
+		}
+
+		for (int i = 0; i < num_combinations; i++)
+		{
+			CPT.push_back(1.0 / nvalues);
+		}
+		listIt->set_CPT(CPT);
+	}
+}
+
+void data_updator()
+{
+}
+
+void CPT_updator(){
+
+};
+
+int main()
+{
+	network Alarm = read_network();
+	Data data = Data(Alarm);
+	return 0;
+}
